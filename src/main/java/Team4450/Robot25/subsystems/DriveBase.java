d2651943488b76062e7433ace86a0bf18f7b7f80
Srivikaat (Vikaat) Ravi <vikaat08@gmail.com>
2025-08-25 15:16:26 -0700

    Update DriveBase.java

--- src/main/java/Team4450/Robot25/subsystems/DriveBase.java
+++ src/main/java/Team4450/Robot25/subsystems/DriveBase.java
@@ -843,6 +843,10 @@ public class DriveBase extends SubsystemBase {
   public void updateOdometryQuest(Pose2d lastPose, double timestamp) {
     odometry.addVisionMeasurement(lastPose, currentRotation);
   }
+
+  public void updateOdometryLimelight(Pose2d lastPose, double timestamp) {
+    odometry.addVisionMeasurement(lastPose, timestamp);
+}
   /**
    * Disables tracking
    */
